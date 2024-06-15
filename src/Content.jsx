832d1914cd68b48131dff853b614cecea011e759
Yogesh7107 <157873734+Yogesh7107@users.noreply.github.com>
2024-06-15 19:50:38 +0530

    update

---
+++ src/Content.jsx
@@ -0,0 +1,13 @@
+function Content(){
+    return(
+        <>
+        <div className="gallery">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzexzFtXBVYuNcKBwGwB_bro4MpVwHPC9kOA&s" alt="" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJsOeXLHugCtkk3k6xIag-ps-wwul3u1xJ1A&s" alt="" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiIDk5KpVsOVI7x-8m1oO_b31-TPfC-rRqAg&s" alt="" />
+        </div>
+        </>
+   
+    )
+}
+export default Content
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+function Header() {
+    return (
+
+    <nav>
+       <div className="menu">
+    
+       <div className="logo"><a href="#">Red And White</a></div>
+            <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Course</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        
+       </div>
+    </nav>
+    )
+}
+export default Header
